0102551351536476b0b8d0ba030d01af43954acc
KwokchunYi <kwokchun.yik@my.jcu.edu.au>
2024-11-24 17:03:36 +0800

    Prac_09

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,10 @@
+from silver_service_taxi import SilverServiceTaxi
+
+def main():
+    taxi = SilverServiceTaxi("Taxi", 405, 2)
+    taxi.drive(18)
+    print(taxi)
+    print(taxi.get_fare())
+
+
+main()
